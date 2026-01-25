154462e059a59384a261051f0d421b22ab637e5b
Arslansaifi012 <devarslan457@gmail.com>
2026-01-25 13:53:53 +0530

    learn parsi data and chunks

---
+++ srver.js
@@ -0,0 +1,35 @@
+
+const http = require('http') ;
+
+const server = http.createServer((req, res)=>{
+    if (req.url === '/send' && req.method === 'POST') {
+
+
+        const body = [] ;
+        req.on('data', (chunks)=>{
+            console.log(chunks,'this is chunks data ');
+            body.push(chunks);
+        }) ;
+      
+        
+
+        req.on('end', ()=>{
+            const fullBody  = Buffer.concat(body).toString() ;
+            // console.log(fullBody);
+            const params = new URLSearchParams(fullBody) ;
+            // console.log(params);
+            const bodyObject = {} ;
+
+            for(const [key, val] of  params.entries()){
+                bodyObject[key] = val ;
+
+            } ;
+            console.log(bodyObject,'this is bodyObject ');
+        
+        })
+    }
+}) ;
+
+server.listen(4000, ()=>{
+    console.log(`server is running 4000 PORT`);
+})
